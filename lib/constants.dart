23647cdb58e2f379265003911c568f384dcbc64e
isaaknewton <isaak.hanimann@gmail.com>
2020-04-14 00:35:46 +0200

    iOS phone authentication works! (added url types)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,2 @@
-const String kDefaultProfilePicUrl = 'www.example.com';
+const String kDefaultProfilePicUrl =
+    'https://firebasestorage.googleapis.com/v0/b/voices-dev.appspot.com/o/default_profile_pic.jpg?alt=media&token=c4cf499e-d0e3-4371-898b-18eed6591f7b';
