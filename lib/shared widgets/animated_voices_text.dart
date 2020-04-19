57da0d2f4614f0162d3835ae274f93512d39e40b
Felix Lunzenfichter <felixlu@student.ethz.ch>
2020-04-19 18:21:56 +0200

    remove busy loop for animation and add listener

--- lib/shared widgets/animated_voices_text.dart
+++ lib/shared widgets/animated_voices_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sequence_animation/flutter_sequence_animation.dart';
+import 'package:voices/constants.dart';
 
 class VoicesAnimated extends StatefulWidget {
   @override
@@ -7,34 +8,19 @@ class VoicesAnimated extends StatefulWidget {
 }
 
 class _State extends State<VoicesAnimated> with SingleTickerProviderStateMixin {
-
   // Animation related fields.
   AnimationController controller;
   SequenceAnimation sequenceAnimation;
-
-  // TODO: is busy loop
-  Future<Null> _playAnimation() async {
-    try {
-      while (true) {
-        await controller.forward().orCancel;
-        await controller.reverse().orCancel;
-      }
-    } on TickerCanceled {
-      // the animation got canceled, probably because we were disposed
-    }
-  }
 
   Widget _buildAnimation(BuildContext context, Widget child) {
     return new AnimatedBuilder(
       builder: (context, child) {
         return new Center(
-          child: new Text(
-              "VOICES",
+          child: new Text("VOICES",
               style: TextStyle(
                 color: sequenceAnimation["color"].value,
                 fontSize: 100.0,
-              )
-          ),
+              )),
         );
       },
       animation: controller,
@@ -43,40 +29,47 @@ class _State extends State<VoicesAnimated> with SingleTickerProviderStateMixin {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
     controller = new AnimationController(vsync: this);
-    
+
     sequenceAnimation = new SequenceAnimationBuilder()
         .addAnimatable(
-        animatable: new ColorTween(begin: Colors.red, end: Colors.yellow),
-        from:  const Duration(seconds: 0),
-        to: const Duration(seconds: 2),
-        tag: "color"
-    ).addAnimatable(
-        animatable: new ColorTween(begin: Colors.yellow, end: Colors.blueAccent),
-        from:  const Duration(seconds: 2),
-        to: const Duration(seconds: 4),
-        tag: "color",
-        curve: Curves.easeOut
-    ).addAnimatable(
-        animatable: new ColorTween(begin: Colors.blueAccent, end: Colors.pink),
-        //  animatable: new Tween<double>(begin: 200.0, end: 40.0),
-        from:  const Duration(seconds: 5),
-        to: const Duration(seconds: 6),
-        tag: "color",
-        curve: Curves.fastOutSlowIn
-    ).animate(controller);
+            animatable: new ColorTween(begin: Colors.red, end: Colors.yellow),
+            from: const Duration(microseconds: 0),
+            to: const Duration(milliseconds: animationspeed),
+            tag: "color")
+        .addAnimatable(
+            animatable:
+                new ColorTween(begin: Colors.yellow, end: Colors.blueAccent),
+            from: const Duration(milliseconds: animationspeed),
+            to: const Duration(milliseconds: animationspeed * 2),
+            tag: "color",
+            curve: Curves.easeOut)
+        .addAnimatable(
+            animatable:
+                new ColorTween(begin: Colors.blueAccent, end: Colors.pink),
+            //  animatable: new Tween<double>(begin: 200.0, end: 40.0),
+            from: const Duration(milliseconds: animationspeed * 2),
+            to: const Duration(milliseconds: animationspeed * 3),
+            tag: "color",
+            curve: Curves.fastOutSlowIn)
+        .animate(controller);
+
+    controller.forward();
+
+    controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    _playAnimation();
-
-    return AnimatedBuilder(
-        animation: controller,
-        builder: _buildAnimation);
+    return AnimatedBuilder(animation: controller, builder: _buildAnimation);
   }
 
   @override
