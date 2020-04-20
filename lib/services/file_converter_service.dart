f23beb17f60f0a08dcf0aee1070b1c6eaeee028a
isaaknewton <isaak.hanimann@gmail.com>
2020-04-20 10:12:26 +0200

    Add ffmpec package

--- lib/services/file_converter_service.dart
+++ lib/services/file_converter_service.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:wave_generator/wave_generator.dart';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 
 class FileConverterService {
   File editedAudioFile;
