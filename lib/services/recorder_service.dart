868b6dfa0c6f4582fe1760564579909b7760b00a
isaaknewton <isaak.hanimann@gmail.com>
2020-05-07 07:48:08 +0200

    Clean up recorder service

--- lib/services/recorder_service.dart
+++ lib/services/recorder_service.dart
@@ -6,19 +6,23 @@ import 'package:path_provider/path_provider.dart';
 import 'package:voices/services/file_converter_service.dart';
 
 class RecorderService with ChangeNotifier {
+  //publicly available
+  Recording
+      currentRecording; //while recording the outside has access to the current recording with this property
+  List<Recording> currentRecordingChunks =
+      []; //if direct send is enabled chunks of the recording are saved in this list
   bool hasPermission = false;
   bool isDirectSendActivated = false;
-  Duration _howMuchOfCurrentMessageSent = Duration(milliseconds: 0);
   RecordingStatus currentStatus = RecordingStatus.Unset;
-  Recording currentRecording;
-  FlutterAudioRecorder _recorder;
   static const Duration DEFAULT_CHUNK_SIZE =
       Duration(seconds: 3); //the last chunk might have a different size
   static const int SAMPLING_FREQUENCY =
       44100; //this is the industry standard for audio files (44100 samples per second)
-  final FileConverterService fileConverterService = FileConverterService();
 
-  List<Recording> currentRecordingChunks = [];
+  //for internal use only
+  FlutterAudioRecorder _recorder;
+  Duration _howMuchOfCurrentMessageSent = Duration(milliseconds: 0);
+  final FileConverterService _fileConverterService = FileConverterService();
 
   activateDirectSend() {
     isDirectSendActivated = true;
@@ -40,10 +44,8 @@ class RecorderService with ChangeNotifier {
         notifyListeners();
       });
 
-      const tickToGenerateChunks = const Duration(
-          seconds:
-              1); //this timer updates the current recording therefore the time should be chosen however much we need it to be updated. If we are just tracking seconds we could let the timer tick less often than 15ms.
-      Timer.periodic(tickToGenerateChunks, (Timer t) async {
+      //this timer adds chunks to the list of chunks if direct send is activated
+      Timer.periodic(DEFAULT_CHUNK_SIZE, (Timer t) async {
         if (currentStatus == RecordingStatus.Stopped) {
           t.cancel();
         }
@@ -62,29 +64,27 @@ class RecorderService with ChangeNotifier {
   }
 
   pauseRecording() async {
-    await _recorder.pause();
     currentStatus = RecordingStatus.Paused;
     notifyListeners();
+    await _recorder.pause();
   }
 
   resumeRecording() async {
-    await _recorder.resume();
     currentStatus = RecordingStatus.Recording;
     notifyListeners();
+    await _recorder.resume();
   }
 
   stopRecording() async {
-    Recording result = await _recorder.stop();
-    if (isDirectSendActivated) {
-      //todo send the last part of the recording as it probably wasn't sent yet
-      _addLastChunk();
-    } else {
-      //todo send whole recording
-      currentRecording = result;
-    }
     isDirectSendActivated = false;
     currentStatus = RecordingStatus.Stopped;
     notifyListeners();
+    Recording result = await _recorder.stop();
+    if (isDirectSendActivated) {
+      _addLastChunk();
+    } else {
+      currentRecording = result;
+    }
   }
 
   _initializeRecorder() async {
@@ -131,12 +131,11 @@ class RecorderService with ChangeNotifier {
   _getChunkAndAddToChunkList(
       {@required Duration startTime, @required Duration endTime}) async {
     //get the audio chunk
-    File chunk = await fileConverterService.createAudioFileChunkFromFile(
+    File chunk = await _fileConverterService.createAudioFileChunkFromFile(
         file: File(currentRecording.path),
         startTime: startTime,
         endTime: endTime,
         chunkFilename: currentRecordingChunks.length.toString());
-    //todo upload chunk to firebase (maybe convert)
 
     //create recording and add it to the list of recordings
     Recording recordingChunk = Recording();
