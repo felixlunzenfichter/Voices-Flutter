09d8d044b3addaac48642d08be63f722ca56218d
Felix Lunzenfichter <felixlu@student.ethz.ch>
2020-05-11 21:59:32 +0200

    speech to text play pause fix.

--- lib/services/speech_to_text_service.dart
+++ lib/services/speech_to_text_service.dart
@@ -1,27 +1,16 @@
 import 'package:speech_recognition/speech_recognition.dart';
 import 'package:flutter/material.dart';
 
-class Language {
-  final String name;
-  final String code;
 
-  const Language(this.name, this.code);
-}
-
-const languages = const [
-  const Language('Español', 'es_ES'),
-  const Language('Francais', 'fr_FR'),
-  const Language('English', 'en_US'),
-  const Language('Pусский', 'ru_RU'),
-  const Language('Italiano', 'it_IT'),
-];
 
 class SpeechToTextService extends ChangeNotifier {
+
+  // Speech recognition object provided by packet.
   SpeechRecognition _speech;
 
-  //todo use those
-  bool _speechRecognitionAvailable = false;
-  int i = 0;
+
+  int unresolvedStartClicks = 0;
+  int pausesToSkip = 0;
 
   // Set to true when we are currently recording.
   bool _isListening = false;
@@ -30,17 +19,23 @@ class SpeechToTextService extends ChangeNotifier {
   // to postpone starting the recorder upon completing the stop.
   bool _shouldPlayOnComplete = false;
 
+  // Transcript since the las time we pressed start.
   String transciptionCurrentRecoringSnippet = '';
+
+  // Transcript of the whole voice message.
   String fullTranscription = '';
 
   //String _currentLocale = 'en_US';
   Language selectedLang = languages.first;
 
+  SpeechToTextService() {
+    activateSpeechRecognizer();
+  }
+
+
   void activateSpeechRecognizer() async {
     _speech = new SpeechRecognition();
-    _speech.setAvailabilityHandler((bool result) async {
-      _speechRecognitionAvailable = result;
-    });
+    _speech.setAvailabilityHandler((bool result) async {});
     _speech.setCurrentLocaleHandler((String locale) {
       selectedLang = languages.firstWhere((l) => l.code == locale);
     });
@@ -49,15 +44,17 @@ class SpeechToTextService extends ChangeNotifier {
       transciptionCurrentRecoringSnippet = text;
       notifyListeners();
     });
+
     _speech.setRecognitionCompleteHandler(() async {
+      unresolvedStartClicks--;
+
+      // This case distinction takes care of potential pending start calls.
       if (_shouldPlayOnComplete) {
         // Don't Play upon next completion.
         _shouldPlayOnComplete = false;
 
         // Save transcript.
-        fullTranscription =
-        "$fullTranscription  $transciptionCurrentRecoringSnippet";
-        transciptionCurrentRecoringSnippet = '';
+        saveTranscipt();
 
         // Start listening.
         _speech.listen(locale: selectedLang.code);
@@ -65,34 +62,38 @@ class SpeechToTextService extends ChangeNotifier {
         // We are done listening.
         _isListening = false;
 
-//        _speech.cancel();
-
         // Save transcript.
-        fullTranscription =
-        "$fullTranscription  $transciptionCurrentRecoringSnippet";
-        transciptionCurrentRecoringSnippet = '';
+        saveTranscipt();
       }
     });
-
-    _speechRecognitionAvailable = await _speech.activate();
+    await _speech.activate();
   }
 
-  SpeechToTextService() {
-    activateSpeechRecognizer();
+
+  // Save transcript.
+  saveTranscipt() {
+    fullTranscription =
+    "$fullTranscription$transciptionCurrentRecoringSnippet\n";
+    transciptionCurrentRecoringSnippet = '';
   }
 
   // Start new Recording or pick up where we left off.
   void start() async {
-    if (!_isListening) {
-      // Start listening.
-      _isListening = true;
-      i += 1;
-      print("start $i");
-      _speech.listen(locale: selectedLang.code);
+    if (unresolvedStartClicks >= 2) {
+      pausesToSkip++;
     } else {
-      // Postpone start listening to onComplete.
-      _shouldPlayOnComplete = true;
+      unresolvedStartClicks++;
+      if (!_isListening) {
+        // Start listening.
+        _isListening = true;
+
+        _speech.listen(locale: selectedLang.code);
+      } else {
+        // Postpone start listening to onComplete.
+        _shouldPlayOnComplete = true;
+      }
     }
+
   }
 
   // Stop recording
@@ -104,8 +105,27 @@ class SpeechToTextService extends ChangeNotifier {
 
   // Pause recording.
   void pause() async {
-    await _speech.stop();
+    if (pausesToSkip > 0) {
+      pausesToSkip--;
+    } else {
+      await _speech.stop();
+    }
   }
 
   void setLanguage() {}
 }
+
+class Language {
+  final String name;
+  final String code;
+
+  const Language(this.name, this.code);
+}
+
+const languages = const [
+  const Language('Francais', 'fr_FR'),
+  const Language('Español', 'es_ES'),
+  const Language('English', 'en_US'),
+  const Language('Pусский', 'ru_RU'),
+  const Language('Italiano', 'it_IT'),
+];
