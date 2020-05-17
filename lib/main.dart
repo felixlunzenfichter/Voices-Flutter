c358b4d89084c4ab9c6ee3631c5ad482bd88ddaf
Felix Lunzenfichter <felixlu@student.ethz.ch>
2020-05-17 11:24:07 +0200

    comments

--- lib/main.dart
+++ lib/main.dart
@@ -18,47 +18,72 @@ import 'services/speech_to_text_service.dart';
 import 'services/file_converter_service.dart';
 
 void main() async {
-  // This app is designed only to work vertically, so we limit
-  // orientations to portrait up and down.
-  WidgetsFlutterBinding.ensureInitialized();
+
+  /// This app is designed only to work vertically, thence we limit orientations to portrait up and down.
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  /// Make sure the glue between the flutter engine and the widget layer is initialized.
+  WidgetsFlutterBinding.ensureInitialized();
+
   return runApp(Voices());
 }
 
 class Voices extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    /// Global state management layer.
     return MultiProvider(
       providers: [
+
+        /// User authentication service.
         ChangeNotifierProvider<AuthService>(
           create: (_) => AuthService(),
         ),
+
+        /// Cloud service for real time data.
         Provider<CloudFirestoreService>(
           create: (_) => CloudFirestoreService(),
         ),
+
+        /// Permanent storage.
         Provider<StorageService>(
           create: (_) => StorageService(),
         ),
+
+        /// Record audio.
         ChangeNotifierProvider<RecorderService>(
           create: (_) => RecorderService(),
         ),
+
+        /// Play audio from local storage.
         Provider<LocalPlayerService>(
           create: (_) => LocalPlayerService(),
         ),
+
+        /// Play audio from the cloud.
         Provider<CloudPlayerService>(
           create: (_) => CloudPlayerService(),
         ),
+
+        /// Handle permission.
         ChangeNotifierProvider<PermissionService>(
           create: (_) => PermissionService(),
         ),
+
+        /// Audio file format conversion.
         Provider<FileConverterService>(
           create: (_) => FileConverterService(),
         ),
+
+        /// Convert speech to text.
         ChangeNotifierProvider<SpeechToTextService>(
           create: (_) => SpeechToTextService(),
         ),
       ],
+
+      /// Detect gestures.
       child: GestureDetector(
         onTap: () {
           FocusScopeNode currentFocus = FocusScope.of(context);
