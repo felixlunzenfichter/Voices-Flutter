14e2b56a3a4e3a804e2f2ca44dd2f6447e65d5a9
isaaknewton <isaak.hanimann@gmail.com>
2020-05-18 02:52:05 +0200

    Add concatenate function to file_converter_service

--- lib/services/file_converter_service.dart
+++ lib/services/file_converter_service.dart
@@ -1,58 +1,86 @@
-//import 'dart:io';
-//import 'package:flutter/foundation.dart';
-//import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_sound/flutter_ffmpeg.dart';
 
 class FileConverterService {
-//  final FlutterFFmpeg _flutterFfmpeg = new FlutterFFmpeg();
-//
-//  Future<File> createAudioFileChunkFromFile(
-//      {@required File file,
-//      @required Duration startTime,
-//      @required Duration endTime,
-//      @required String chunkFilename}) async {
-//    assert(endTime > startTime);
-//    assert(file != null);
-//
-//    String path = file.parent.path + "/$chunkFilename.aac";
-//    File newFile = File(path);
-//    if (await newFile.exists()) {
-//      await newFile.delete();
-//    }
-//
-//    //clip out audio starting from startTime to endTime
-//    Duration lengthOfChunk = endTime - startTime;
-//    String start = _convertDurationToExpectedFormat(duration: startTime);
-//    String length = _convertDurationToExpectedFormat(duration: lengthOfChunk);
-//
-//    if (await _flutterFfmpeg
-//            .execute("-ss $start -t $length -i ${file.path} $path") ==
-//        -1) {
-//      print("An error occured while executing ffmpeg trim command");
-//    } else {
-//      print("ffmpeg trimming exited successfully");
-//    }
-//    return File(path);
-//  }
-//
-//  String _convertDurationToExpectedFormat({@required Duration duration}) {
-//    return "${_twoDigits(duration.inHours)}:${_twoDigits(duration.inMinutes)}:${_twoDigits(duration.inSeconds)}.${_fourDigits(duration.inMilliseconds)}";
-//  }
-//
-//  String _twoDigits(int n) {
-//    if (n >= 10) return "$n";
-//    return "0$n";
-//  }
-//
-//  String _fourDigits(int n) {
-//    if (n < 10) {
-//      return "000$n";
-//    } else if (n < 100) {
-//      return "00$n";
-//    } else if (n < 1000) {
-//      return "0$n";
-//    } else if (n > 1000) {
-//      return "$n";
-//    }
-//    return n.toString();
-//  }
+  final FlutterFFmpeg _flutterFfmpeg = new FlutterFFmpeg();
+
+  Future<File> concatenate(
+      {@required File file1,
+      @required File file2,
+      @required String newFilename}) async {
+    assert(file1 != null);
+    assert(file2 != null);
+    assert(newFilename != null);
+
+    String parentDirectoryPath = file1.parent.path;
+
+    /// The ffmpeg command needs a text file which specifies all the file paths of the files it needs to concatenate
+    String textFileContent = "file '${file1.path}'\nfile '${file2.path}'";
+    String textFilePath = parentDirectoryPath + "/filesToConcatenate.txt";
+    File textFile =
+        await File(textFilePath).writeAsString(textFileContent, flush: true);
+
+    String newFilePath = parentDirectoryPath + "/$newFilename.wav";
+
+    if (await _flutterFfmpeg.execute(
+            "-f concat -safe 0 -i ${textFile.path} -c copy $newFilePath") ==
+        -1) {
+      print("An error occured while executing ffmpeg concat command");
+    } else {
+      print("ffmpeg concatenating exited successfully");
+    }
+    return File(newFilePath);
+  }
+
+  Future<File> createAudioFileChunkFromFile(
+      {@required File file,
+      @required Duration startTime,
+      @required Duration endTime,
+      @required String chunkFilename}) async {
+    assert(endTime > startTime);
+    assert(file != null);
+
+    String path = file.parent.path + "/$chunkFilename.aac";
+    File newFile = File(path);
+    if (await newFile.exists()) {
+      await newFile.delete();
+    }
+
+    //clip out audio starting from startTime to endTime
+    Duration lengthOfChunk = endTime - startTime;
+    String start = _convertDurationToExpectedFormat(duration: startTime);
+    String length = _convertDurationToExpectedFormat(duration: lengthOfChunk);
+
+    if (await _flutterFfmpeg
+            .execute("-ss $start -t $length -i ${file.path} $path") ==
+        -1) {
+      print("An error occured while executing ffmpeg trim command");
+    } else {
+      print("ffmpeg trimming exited successfully");
+    }
+    return File(path);
+  }
+
+  String _convertDurationToExpectedFormat({@required Duration duration}) {
+    return "${_twoDigits(duration.inHours)}:${_twoDigits(duration.inMinutes)}:${_twoDigits(duration.inSeconds)}.${_fourDigits(duration.inMilliseconds)}";
+  }
+
+  String _twoDigits(int n) {
+    if (n >= 10) return "$n";
+    return "0$n";
+  }
+
+  String _fourDigits(int n) {
+    if (n < 10) {
+      return "000$n";
+    } else if (n < 100) {
+      return "00$n";
+    } else if (n < 1000) {
+      return "0$n";
+    } else if (n > 1000) {
+      return "$n";
+    }
+    return n.toString();
+  }
 }
