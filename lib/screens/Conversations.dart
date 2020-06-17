d3e6305c6c071cdfb2b525d5235b73f53f099b7a
Felix Lunzenfichter <felixlu@student.ethz.ch>
2020-06-17 16:54:53 +0200

    redo conversations documentation

--- lib/screens/Conversations.dart
+++ lib/screens/Conversations.dart
@@ -5,25 +5,33 @@ import 'package:voices/models/user.dart';
 import 'package:voices/models/chat.dart';
 import 'package:voices/services/auth_service.dart';
 import 'package:voices/services/cloud_firestore_service.dart';
-//import 'package:voices/shared_widgets/profile_picture.dart';
+import 'package:voices/shared_widgets/profile_picture.dart';
 import 'chat_screen/chat_screen.dart';
 import 'search_users_screen.dart';
 import 'package:voices/shared_widgets/custom_card.dart';
 
+/// See active chats.
 class ChatsTab extends StatefulWidget {
   @override
   _ChatsTabState createState() => _ChatsTabState();
 }
 
 class _ChatsTabState extends State<ChatsTab> {
+
+  /// This firebase stream provides the active conversations of the current user.
   Stream<List<Chat>> chatStream;
 
+  /// Initialize the [chatStream].
   @override
   void initState() {
     super.initState();
+
+    /// - Services -
     final cloudFirestoreService =
-        Provider.of<CloudFirestoreService>(context, listen: false);
+    Provider.of<CloudFirestoreService>(context, listen: false);
     final authService = Provider.of<LoggedInUserService>(context, listen: false);
+
+    /// Get the stream of active conversations for the current user.
     chatStream = cloudFirestoreService.getChatsStream(
         loggedInUid: authService.loggedInUser.uid);
   }
@@ -34,6 +42,8 @@ class _ChatsTabState extends State<ChatsTab> {
       appBar: AppBar(
         title: Text("Chats"),
         actions: <Widget>[
+
+          /// Add a new chat to the active ones.
           CupertinoButton(
             child: Icon(
               Icons.add_circle_outline,
@@ -49,36 +59,49 @@ class _ChatsTabState extends State<ChatsTab> {
               );
             },
           ),
+
         ],
       ),
+
       body: SafeArea(
         bottom: false,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
+
+              /// List the available chats.
+              /// TODO: This is a stateless widget. What if I am on this screen and I receive a message from a user not contained in the chatStream?
                 child: StreamBuilder<List<Chat>>(
                     stream: chatStream,
                     builder: (context, snapshot) {
+                      /// TODO: If connectionState is none then we should tell the user that he is not connected to the internet.
                       if (snapshot.connectionState == ConnectionState.waiting ||
                           snapshot.connectionState == ConnectionState.none) {
                         return CupertinoActivityIndicator();
                       }
+
+                      /// Convert this stream to an editable list.
                       List<Chat> chats = List.from(snapshot.data ??
-                          []); // to convert it to editable list
+                          []);
+
+                      /// Sort chats according to time of last message.
                       chats.sort((chat1, chat2) => (chat2.lastMessageTimestamp)
                           .compareTo(chat1.lastMessageTimestamp));
 
+                      /// Inform that no active chats exist.
                       if (chats.isEmpty) {
                         return Center(child: Text("You have no open chats"));
                       }
+
+                      /// Display list of chat tabs.
                       return ListView.builder(
                           itemCount: chats.length,
                           itemBuilder: (context, index) {
-                            return ChatItem(
-                              chat: chats[index],
-                            );
+                            return
+                              ChatItem(chat: chats[index]);
                           });
+
                     }))
           ],
         ),
@@ -87,6 +110,7 @@ class _ChatsTabState extends State<ChatsTab> {
   }
 }
 
+/// This widget displays a chat tab.
 class ChatItem extends StatefulWidget {
   final Chat chat;
 
@@ -102,12 +126,18 @@ class _ChatItemState extends State<ChatItem> {
   @override
   void initState() {
     super.initState();
+
+    /// Services
     final cloudFirestoreService =
-        Provider.of<CloudFirestoreService>(context, listen: false);
+    Provider.of<CloudFirestoreService>(context, listen: false);
     final authService = Provider.of<LoggedInUserService>(context, listen: false);
+
+    /// Get the uid of the other user. (todo: can this be cleaner?)
     String otherUserUid = widget.chat.uidsOfMembers
         .where((uid) => uid != authService.loggedInUser.uid)
         .toList()[0];
+
+    /// Fetch the other User.
     otherUserFuture = cloudFirestoreService.getUserWithUid(uid: otherUserUid);
   }
 
@@ -130,19 +160,28 @@ class _ChatItemState extends State<ChatItem> {
           );
         }
         User otherUser = snapshot.data;
+
+        /// Show a chat tab.
+        /// TODO: listen to what the other one has to say and automatically enter chat and be ready to respond. Just play last thing that has not been listened to. Quick start of voice communication. Show new messages. Show time of unlistened material.
         return CustomCard(
-          leading:
-              Container(), //ProfilePicture(imageUrl: otherUser.imageUrl, radius: 30),
+
+          /// display profile picture.
+          leading: ProfilePicture(imageUrl: otherUser.imageUrl, radius: 30),
+
           middle: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
+
+                  /// Show Username
                   Text(
                     otherUser.username,
                     overflow: TextOverflow.ellipsis,
                   ),
+
+                  /// Show time of last message.
                   Expanded(
                     child: Text(
                       widget.chat.lastMessageTimestamp.toString(),
@@ -155,6 +194,8 @@ class _ChatItemState extends State<ChatItem> {
               SizedBox(
                 height: 6,
               ),
+
+              /// Display last message.
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -169,6 +210,8 @@ class _ChatItemState extends State<ChatItem> {
               ),
             ],
           ),
+
+          /// Jump into chat.
           onPress: () {
             Navigator.of(context, rootNavigator: true).push(
               CupertinoPageRoute<void>(
@@ -185,5 +228,4 @@ class _ChatItemState extends State<ChatItem> {
       },
     );
   }
-  
 }
